a747185507adef60a5df1f64f18580ad418ab4a8
hitak77 <hitak77@naver.com>
2023-02-20 22:20:24 +0900

    양꼬치

---
+++ lambskewers/src/test/kotlin/lambskewers.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(n: Int, k: Int): Int {
+        var answer: Int = 0
+        val n1: Int = n / 10
+        answer = n*12000 + (k-n1)*2000
+        return answer
+    }
+}
