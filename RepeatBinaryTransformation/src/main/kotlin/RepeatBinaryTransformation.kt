ed7494850ce2bf42585d487c5e31589a223d754d
hitak77 <hitak77@naver.com>
2023-03-08 10:09:03 +0900

    level 2) 이진 변환 반복하기

--- RepeatBinaryTransformation/src/main/kotlin/RepeatBinaryTransformation.kt
+++ RepeatBinaryTransformation/src/main/kotlin/RepeatBinaryTransformation.kt
@@ -1,5 +1,21 @@
 class Solution {
     fun solution(s: String): IntArray {
+        var str = s
+        var deleteCount = 0
+        var count = 0
+        while(str != "1"){
+            val zeroCount = str.count{it == '0'}
+            deleteCount += zeroCount
+            str = Integer.toBinaryString(str.length - zeroCount)
+            count++
+        }
+        return intArrayOf(count, deleteCount)
+    }
+}
+
+//===============================================================
+class Solution {
+    fun solution(s: String): IntArray {
         var answer: IntArray = intArrayOf()
         var str = s
         var deleteCount: Int = 0
@@ -31,3 +47,4 @@ class Solution {
 
     }
 }
+//==========================================================
