f9e018335415ebe0364fcc8fb932360e5c20ffe0
hitak77 <hitak77@naver.com>
2023-03-08 10:04:17 +0900

    level 2) 이진 변환 반복하기 (시간초과 ver)

---
+++ RepeatBinaryTransformation/src/main/kotlin/RepeatBinaryTransformation.kt
@@ -0,0 +1,33 @@
+class Solution {
+    fun solution(s: String): IntArray {
+        var answer: IntArray = intArrayOf()
+        var str = s
+        var deleteCount: Int = 0
+        var count: Int = 0
+        while(str != "1"){
+            val strOriginal = str.length
+            str = str.replace("0","")
+            deleteCount += strOriginal - s.length
+            str = decimalToBinary(s.length)
+            count++
+        }
+        answer = intArrayOf(deleteCount, count)
+        return answer
+    }
+
+    fun decimalToBinary(decimal: Int): String {
+        var quotient = decimal
+        var binary = ""
+        while(quotient > 0){
+            val remainder = quotient %2
+            binary = "$remainder$binary"
+            quotient /= 2
+        }
+        return if(binary.isEmpty()){
+            "0"
+        } else {
+            binary
+        }
+
+    }
+}
