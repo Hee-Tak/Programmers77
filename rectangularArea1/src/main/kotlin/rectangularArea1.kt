bb6b553e4da596591533afa989dd5e089da9cb0d
hitak77 <hitak77@naver.com>
2023-03-09 23:58:41 +0900

    level 0) 직사각형 넓이 구하기 (map, 람다식 활용)

--- rectangularArea1/src/main/kotlin/rectangularArea1.kt
+++ rectangularArea1/src/main/kotlin/rectangularArea1.kt
@@ -1,10 +1,20 @@
 import kotlin.math.*
-
+/*
 class Solution {
     fun solution(dots: Array<IntArray>): Int {
         val width = dots.map { it[0] }.max() - dots.map { it[0] }.min()
         val height = dots.map { it[1] }.max() - dots.map { it[1] }.min()
         val area = width * height
         return area
+    }
+}
+*/
+
+
+class Solution {
+    fun solution(dots: Array<IntArray>): Int {
+        val width = dots.map { it[0] }.maxOrNull()!! - dots.map { it[0] }.minOrNull()!!
+        val height = dots.map { it[1] }.maxOrNull()!! - dots.map { it[1] }.minOrNull()!!
+        return width * height
     }
 }
