1ede2d462240b7df9b9b6a6a246a47022b6a2f68
hitak77 <hitak77@naver.com>
2023-03-10 10:27:38 +0900

    level 2) 최댓값과 최솟값 (풀이방법 2개)

--- minOrMax/src/main/kotlin/maxAndMin.kt
+++ minOrMax/src/main/kotlin/maxAndMin.kt
@@ -6,3 +6,21 @@ class Solution {
         return "$min $max"
     }
 }
+
+class Solution {
+    fun solution(s: String): String {
+        //val num = s.split(" ").map{it.toInt()}
+        //val min = num.minOrNull() ?: return ""
+        //val max = num.maxOrNull() ?: return ""
+        //return "$min $max"
+
+        val arr = s.split(" ")
+        val num = IntArray(arr.size)
+        for(i in arr.indices){
+            num[i] = arr[i].toInt()
+        }
+        val min = num.minOrNull() ?: return ""
+        val max = num.maxOrNull() ?: return ""
+        return "$min $max"
+    }
+}
