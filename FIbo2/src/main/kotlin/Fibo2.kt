f5edee5bd18413dd939989f586bf35e8df38b4a4
hitak77 <hitak77@naver.com>
2023-03-21 10:46:17 +0900

    level 2) 피보나치 수열 (재귀함수는 문제가 좀 있음)

--- FIbo2/src/main/kotlin/Fibo2.kt
+++ FIbo2/src/main/kotlin/Fibo2.kt
@@ -14,3 +14,22 @@ class Solution {
     }
 }
 
+
+
+class Solution {
+    fun solution(n: Int): Int {
+        val mod = 1234567
+        var a = 0
+        var b = 1
+        for(i in 2..n){
+            val c = (a+b) % mod
+            a = b
+            b = c
+        }
+        return b
+    }
+}
+
+
+
+
