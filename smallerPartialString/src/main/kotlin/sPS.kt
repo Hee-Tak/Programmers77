388b4c4d437d103c05099d11f44bbc22cad0f1de
hitak77 <hitak77@naver.com>
2023-04-01 02:08:37 +0900

    level 1) 크기가 작은 부분 문자열

---
+++ smallerPartialString/src/main/kotlin/sPS.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun solution(t: String, p: String): Int {
+        var answer: Int = 0
+        val numP = p.toLong() // for문에서 여러번 형변환 하는거보다 여기서 미리 해두기
+        val lenT = t.length //미리 길이 받아놓기 for문에서 indices 로 for문과 substring에서의 중복 제거
+        val lenP = p.length
+        for(s in 0 until lenT - lenP+1){ // if(s+p.length > t.length) break 까지 고려
+
+            val s1 = t.substring(s, lenP+s).toLong()
+            if(s1 <= numP){
+                answer++
+            }
+        }
+        return answer
+    }
+}
+// 주의해야할점
+//1 <= p <= 18 ---> Int 를 넘어서므로 런타임 에러가 뜬다면 크기 비교를 long 타입으로..
