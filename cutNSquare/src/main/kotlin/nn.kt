f5ba8c74356e232c3a20b30545c6c98c159093a4
hitak77 <hitak77@naver.com>
2023-04-14 14:34:54 +0900

    level 2) n^2 배열 자르기

--- cutNSquare/src/main/kotlin/nn.kt
+++ cutNSquare/src/main/kotlin/nn.kt
@@ -35,3 +35,30 @@ class Solution {
         return answer
     }
 }//===========반은 맞고 반은 틀림
+class Solution {
+    fun solution(n: Int, left: Long, right: Long): IntArray {
+        val left_ = left.toInt()
+        val right_ = right.toInt()
+        return IntArray(right_-left_+1){ i -> Math.max(((i+left_)/n)1, ((i+left_)%n)+1)}
+    }
+}//==============얘도 마찬가지
+class Solution {
+    fun solution(n: Int, left: Long, right: Long): IntArray {
+        val left_ = left.toInt()
+        val right_ = right.toInt()
+        return IntArray(right_-left_+1){ i -> Math.max(((i+left_)/n)+1, ((i+left_)%n)+1)}
+    }
+}//==============이거도 안돼?
+
+class Solution {
+    fun solution(n: Int, left: Long, right: Long): IntArray {
+        val answer = IntArray((right - left + 1).toInt()) { i ->
+            val row = ((i + left) / n).toInt() + 1 // 행
+            val col = ((i + left) % n).toInt() + 1 // 열
+            maxOf(row, col)
+        }
+        return answer
+    }
+}//==== 이거는 정답임
+//눈으로 보이는건 형변환을 언제 어디서 붙여주느냐... 그 차이 밖에 안보이는데 그게 클 수 도 있긴하지
+// 불필요하게 형변환 많이하는부분에다가 꽂아서 그런가?
