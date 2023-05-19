a5fca9266aff63e23c3b774c70f29d13d2da15a4
hitak77 <hitak77@naver.com>
2023-05-19 09:16:33 +0900

    level 1) 추억 점수

---
+++ nostalgiaScore/src/main/kotlin/NS.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun solution(name: Array<String>, yearning: IntArray, photo: Array<Array<String>>): IntArray {
+        var answer: IntArray = intArrayOf()
+        val map = mutableMapOf<String, Int>()
+        for(i in 0..name.size-1){
+            map.put(name[i], yearning[i])
+        }
+        for(ph in photo){
+            var sum = 0
+            for(key in map.keys){
+                if(key in ph){
+                    sum += map[key] ?: 0 //value
+                }
+            }
+            answer += sum
+        }
+        return answer
+    }
+}
