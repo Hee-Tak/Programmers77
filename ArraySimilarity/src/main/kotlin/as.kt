3fcd6f12eb9ca4c1252cadd4efb39968d300837e
hitak77 <hitak77@naver.com>
2023-06-05 22:13:12 +0900

    level 0) 배열의 유사도

---
+++ ArraySimilarity/src/main/kotlin/as.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(s1: Array<String>, s2: Array<String>): Int {
+        var answer: Int = 0
+        for(i in s1){
+            if(s2.contains(i)){
+                answer++
+            }
+        }
+        return answer
+    }
+}
