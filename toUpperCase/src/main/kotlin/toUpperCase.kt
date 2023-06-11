b79457e7f2f8ad222f31554f19750eecaf05f372
hitak77 <hitak77@naver.com>
2023-06-12 05:40:10 +0900

    level 0) 대문자로 바꾸기

---
+++ toUpperCase/src/main/kotlin/toUpperCase.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(myString: String): String {
+        return myString.toUpperCase()
+    }
+}
+
+/**
+ * level 0) 대문자로 바꾸기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181877
+ */
