f76007232309de4beec4ef5d23a503ead5626caa
hitak77 <hitak77@naver.com>
2023-06-12 05:26:27 +0900

    level 0) 문자열로 변환

---
+++ IntToString/src/main/kotlin/ITS.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(n: Int): String {
+        return n.toString()
+    }
+}
+/**
+ * level 0) 문자열로 변환
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181845
+ */
