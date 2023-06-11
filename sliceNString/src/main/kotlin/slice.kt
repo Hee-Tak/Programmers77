4056d6249904517d29002d83bb580e9aad1c2ff6
hitak77 <hitak77@naver.com>
2023-06-12 02:04:40 +0900

    level 0) 문자열의 앞의 n글자

---
+++ sliceNString/src/main/kotlin/slice.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(my_string: String, n: Int): String {
+        var answer: String = ""
+        return my_string.slice(0..(n-1))
+    }
+}
+
+/**
+ * level 0) 문자열의 앞의 n글자
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181907
+ */
