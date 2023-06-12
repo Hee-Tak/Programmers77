390e86b95253a326a2a05fc8c7f6d6131c12e81a
hitak77 <hitak77@naver.com>
2023-06-13 08:11:20 +0900

    level 0) 소문자로 바꾸기

---
+++ toLowerCase/src/main/kotlin/toLowerCase.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(myString: String): String {
+        return myString.toLowerCase()
+    }
+}
+/**
+ * level 0) 소문자로 바꾸기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181876
+ */
