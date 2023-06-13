7eddcbe0648da3808d398e13811ae6ae7e9624c5
hitak77 <hitak77@naver.com>
2023-06-13 16:48:09 +0900

    level 0) n의 배수

---
+++ Ns_multiple/src/main/kotlin/n.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(num: Int, n: Int): Int {
+        return if(num % n == 0){
+            1
+        } else {
+            0
+        }
+    }
+}
+/**
+ * level 0) n의 배수
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181937
+ */
