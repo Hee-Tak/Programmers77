5abae87ff3191e3bd5f6f7274f33e14dd8877bf0
hitak77 <hitak77@naver.com>
2023-06-14 21:47:01 +0900

    level 0) 정수 찾기

---
+++ FindN/src/main/kotlin/findN.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(num_list: IntArray, n: Int): Int {
+
+        return if(n in num_list){
+            1
+        } else {
+            0
+        }
+    }
+}
+/**
+ * level 0) 정수 찾기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181840
+ */
