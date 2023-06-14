91b249bd69e6c329c217bc4ad15a193cea085d47
hitak77 <hitak77@naver.com>
2023-06-14 21:54:27 +0900

    level 0) rny_string

---
+++ mToRN/src/main/kotlin/rny_string.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(rny_string: String): String {
+        var answer: String = ""
+        return rny_string.replace("m", "rn")
+    }
+}
+/**
+ * level 0) rny_string
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181863
+ */
