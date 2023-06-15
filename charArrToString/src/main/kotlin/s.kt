bf1196be35c9638eb96a06c0a351963d481193a1
hitak77 <hitak77@naver.com>
2023-06-15 20:08:37 +0900

    level 0) 문자 리스트를 문자열로 변환하기

---
+++ charArrToString/src/main/kotlin/s.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(arr: Array<String>): String {
+        var answer: String = ""
+        for(i in arr){
+            answer += i
+        }
+        return answer
+    }
+}
+/**
+ * level 0) 문자 리스트를 문자열로 변환하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181941
+ */
