c6fae7484fafc432f7cd1c95cf76bed250fd1429
hitak77 <hitak77@naver.com>
2023-06-15 19:07:31 +0900

    level 0) A 강조하기

---
+++ EmphasizingA/src/main/kotlin/A.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(myString: String): String {
+        var answer: String = ""
+        return myString.toLowerCase().replace("a", "a".toUpperCase())
+    }
+}
+
+/**
+ * level 0) A 강조하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181874
+ */
