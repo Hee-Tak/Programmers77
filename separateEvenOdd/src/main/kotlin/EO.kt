b512bbaf6f68f7070c352bcf266e99dc2aafb6dd
hitak77 <hitak77@naver.com>
2023-06-20 07:00:04 +0900

    level 0) 홀짝 구분하기

---
+++ separateEvenOdd/src/main/kotlin/EO.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    val a = readLine()!!.toInt()
+
+    if(a % 2 == 0){
+        println("$a is even")
+    } else{
+        println("$a is odd")
+    }
+
+}
+
+
+/**
+ * level 0) 홀짝 구분하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181944
+ */
