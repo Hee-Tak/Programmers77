d00363d63be02b9485614f3474dcef6a2af6b2bf
hitak77 <hitak77@naver.com>
2023-07-16 16:47:08 +0900

    level 0) 직각삼각형 출력하기

---
+++ rightTriangle/src/main/kotlin/rightTriangle.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    val (n) = readLine()!!.split(' ').map(String::toInt)
+    //println(n)
+    for(k in 1..n){
+        for(s in 1..k){
+            print("*")
+        }
+        println()
+    }
+}
+
+/**
+ * level 0) 직각삼각형 출력하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120823
+ */
