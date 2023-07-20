adf185c431b8001de36e4fcba745e65400668d53
hitak77 <hitak77@naver.com>
2023-07-21 08:39:24 +0900

    level 0) 간단한 논리 연산

---
+++ simpleLogicalOperation/src/main/kotlin/simple.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(x1: Boolean, x2: Boolean, x3: Boolean, x4: Boolean): Boolean {
+        return (x1 || x2) && (x3 || x4)
+    }
+}
+
+/**
+ * level 0) 간단한 논리 연산
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181917
+ */
