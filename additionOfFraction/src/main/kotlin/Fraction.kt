f1a4ed00ac7a253dd733fd4534c5b94852b75bdf
hitak77 <hitak77@naver.com>
2023-07-29 14:23:05 +0900

    level 0) 분수의 덧셈

--- additionOfFraction/src/main/kotlin/Fraction.kt
+++ additionOfFraction/src/main/kotlin/Fraction.kt
@@ -43,3 +43,34 @@ class Solution {
  * 테스트5랑 13만 자꾸 틀림...
  *
  */
+
+class Solution {
+    fun solution(numer1: Int, denom1: Int, numer2: Int, denom2: Int): IntArray {
+        val commonDenom = denom1 * denom2 // 두 분수의 분모를 곱해서 통분
+        val newNumer1 = numer1 * denom2 // 분자를 통분한 분수 1의 분자
+        val newNumer2 = numer2 * denom1 // 분자를 통분한 분수 2의 분자
+        val sumNumer = newNumer1 + newNumer2 // 두 분수의 분자를 더한 값
+
+        // 더한 결과의 분자와 분모의 최대공약수를 구함
+        val gcdValue = gcd(sumNumer, commonDenom)
+
+        // 기약 분수로 만들어줌
+        val reducedNumer = sumNumer / gcdValue
+        val reducedDenom = commonDenom / gcdValue
+
+        return intArrayOf(reducedNumer, reducedDenom)
+    }
+
+    // 최대공약수 계산 함수
+    private fun gcd(a: Int, b: Int): Int {
+        return if (b == 0) a else gcd(b, a % b)
+    }
+}
+
+/**
+ * level 0) 분수의 덧셈
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120808
+ *
+ * 이건 수정한 정답
+ *
+ */
