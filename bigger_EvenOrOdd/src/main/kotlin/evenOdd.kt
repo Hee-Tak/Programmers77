e1f91c392ffd15629a25ff7aae70e93521a9acb8
hitak77 <hitak77@naver.com>
2023-07-31 07:07:19 +0900

    level 0) 홀수 vs 짝수

--- bigger_EvenOrOdd/src/main/kotlin/evenOdd.kt
+++ bigger_EvenOrOdd/src/main/kotlin/evenOdd.kt
@@ -1,3 +1,5 @@
+
+//첫번쨰 방법 (생각해본거)
 class Solution {
     fun solution(num_list: IntArray): Int {
         var answer: Int = 0
@@ -16,6 +18,33 @@ class Solution {
         }
     }
 }
+
+
+// 두번째
+class Solution {
+    fun solution(num_list: IntArray): Int {
+        var answer: Int = 0
+        var even: Int = 0
+        var odd: Int = 0
+        for(i in 0..num_list.size-1){
+            when(i%2){
+                0 -> {
+                    even += num_list[i]
+                }
+                1 -> {
+                    odd += num_list[i]
+                }
+            }
+        }
+        if(even >= odd){
+            answer = even
+        } else {
+            answer = odd
+        }
+        return answer
+    }
+}
+
 /**
  * level 0) 홀수 vs 짝수
  * https://school.programmers.co.kr/learn/courses/30/lessons/181887
