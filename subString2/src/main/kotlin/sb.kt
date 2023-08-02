215d1fe8e5e8a3002fdb4d80d29f6cbceb660fdb
hitak77 <hitak77@naver.com>
2023-08-03 05:09:14 +0900

    level 0) 부분 문자열

---
+++ subString2/src/main/kotlin/sb.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(str1: String, str2: String): Int {
+        return if(str1 in str2) 1 else 0
+    }
+}
+
+/**
+ * level 0) 부분 문자열
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181842
+ */
