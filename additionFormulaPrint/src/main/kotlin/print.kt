37123fad8061438e4544a279a6c6a89421b26fcc
hitak77 <hitak77@naver.com>
2023-08-05 05:43:29 +0900

    level 0) 덧셈식 출력하기

---
+++ additionFormulaPrint/src/main/kotlin/print.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val (a, b) = readLine()!!.split(' ').map(String::toInt)
+    println("$a + $b = ${a+b}")
+}
+
+/**
+ * level 0) 덧셈식 출력하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181947
+ */
