f5b599141d01d54fbac29c1ec682a06fbb56ae8f
hitak77 <hitak77@naver.com>
2023-09-10 00:50:32 +0900

    level 0) 문자열 정렬하기 (2) (java)

--- sortedString/src/main/kotlin/sorted.java
+++ sortedString/src/main/kotlin/sorted.java
@@ -10,6 +10,21 @@ class Solution {
     }
 }
 
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        char[] charArray = my_string.toLowerCase().toCharArray();
+        Arrays.sort(charArray);
+        return new String(charArray);
+    }
+}
+
+
+
+
+
+
 /**
  * level 0) 문자열 정렬하기 (2)
  * https://school.programmers.co.kr/learn/courses/30/lessons/120911?language=java
