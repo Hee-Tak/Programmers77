13cc3007a59537e6daeed773cabc84cc55f39505
hitak77 <hitak77@naver.com>
2023-09-10 00:47:41 +0900

    level 0) 문자열 정렬하기 (2)

---
+++ sortedString/src/main/kotlin/sorted.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(my_string: String): String {
+        return my_string.toLowerCase().toCharArray().sorted().joinToString("")
+    }
+}
+/**
+ * level 0) 문자열 정렬하기 (2)
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120911?language=kotlin
+ */
