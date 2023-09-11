936f47587f55ca4dfd78f5b0318c3c378a579439
hitak77 <hitak77@naver.com>
2023-09-11 12:43:54 +0900

    level 0) 수 조작하기 2

---
+++ controlNumber_d_wasd/src/main/kotlin/control.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun solution(numLog: IntArray): String {
+        var answer: String = ""
+
+        for(i in 0 until numLog.size-1){
+            val d = numLog[i+1] - numLog[i]
+            when(d) {
+                1 -> answer += "w"
+                -1 -> answer += "s"
+                10 -> answer += "d"
+                -10 -> answer += "a"
+            }
+        }
+
+        return answer
+    }
+}
+
+/**
+ * level 0) 수 조작하기 2
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181925
+ */
