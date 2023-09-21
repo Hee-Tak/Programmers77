ca05838bb195232ec8ed72233e581cf7a4de4009
hitak77 <hitak77@naver.com>
2023-09-22 03:25:30 +0900

    level 0) 몫 구하기 (java)

---
+++ Share_java/src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
+
+
+/**
+ * level 0) 몫 구하기 (java)
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120805
+ */
---
+++ Share_java/src/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args){
+
+        Solution solution = new Solution();
+        int num1 = 144;
+        int num2 = 12;
+        int result = solution.solution(num1, num2);
+
+        System.out.println("num1 / num2 = " + result);
+
+    }
+}
