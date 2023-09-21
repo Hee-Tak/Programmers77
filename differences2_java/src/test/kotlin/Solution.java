0b4c4ae13ba4fed05920d62a174b4150485c642e
hitak77 <hitak77@naver.com>
2023-09-21 22:42:53 +0900

    level 0) 두 수의 차 (java)

---
+++ differences2_java/src/test/kotlin/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+}
+
+
+/**
+ * level 0) 두 수의 차
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120803?language=java
+ *
+ * 한국주택금융공사 필기 대비 java 학습
+ */
---
+++ differences2_java/src/test/kotlin/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+
+        int num1 = 10;
+        int num2 = 5;
+
+        int result = solution.solution(num1, num2);
+
+        System.out.println("num1 - num2 = " + result);
+    }
+}
+
