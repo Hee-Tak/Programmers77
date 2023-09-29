c6f371073698d8945c2757f3eaf7affc7fe2d678
hitak77 <hitak77@naver.com>
2023-09-29 20:59:07 +0900

    level 1) 짝수와 홀수 (java)

---
+++ OddAndEven/src/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+
+        if(num%2==0){
+            answer = "Even";
+        } else {
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
+
+/**
+ * level 1) 짝수와 홀수
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12937
+ */
