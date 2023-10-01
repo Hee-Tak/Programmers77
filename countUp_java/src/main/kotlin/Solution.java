2811d0ea2266507e0b438893b259d404a3f9ae12
hitak77 <hitak77@naver.com>
2023-10-02 03:25:15 +0900

    level 0) 카운트 업 (java)

---
+++ countUp_java/src/main/kotlin/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = {};
+        int N = end_num - start_num + 1;
+        answer = new int[N];
+        for(int i = start_num; i <= end_num; i++){
+            answer[i-start_num] = i;
+        }
+        return answer;
+    }
+}
+
+/**
+ * level 0) 카운트 업
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181920
+ */
