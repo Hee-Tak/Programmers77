60fca34780afe20f2d1bb484727fffc91deff4d9
hitak77 <hitak77@naver.com>
2023-10-03 18:12:16 +0900

    level 0) 문자열안에 문자열

---
+++ StringInString/src/main/kotlin/Solution2.java
@@ -0,0 +1,16 @@
+class Solution2 {
+    public int solution(String str1, String str2) {
+
+        if(str1.contains(str2)){
+            return 1;
+        } else {
+            return 2;
+        }
+
+    }
+}
+
+/**
+ * level 0) 문자열안에 문자열
+ * 
+ */
