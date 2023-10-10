827e694aa7ae7a858b0b0cdd2719ee3922fdbdd0
hitak77 <hitak77@naver.com>
2023-10-10 12:58:52 +0900

    level 0) 특수문자 출력하기

---
+++ specialLetterPrint/src/main/kotlin/Solution.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val str: String = "!@#$%^&*(\\\'\"<>?:;"
+    println(str)
+}
+
+/**
+ * level 0) 특수문자 출력하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181948
+ *
+ * 역슬래쉬 \ 이거 사용
+ */
