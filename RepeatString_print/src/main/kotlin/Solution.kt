1402cf7696b02b6d8d369564bd79c63ae003b637
hitak77 <hitak77@naver.com>
2023-10-11 10:50:33 +0900

    level 0) 문자열 반복해서 출력하기

---
+++ RepeatString_print/src/main/kotlin/Solution.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    val input = readLine()!!.split(' ')
+    val s1 = input[0]
+    val a = input[1]!!.toInt()
+
+    for(i in 1..a){
+        print("${s1}")
+    }
+}
+
+/**
+ * level 0) 문자열 반복해서 출력하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181950
+ */
