8243428db36e053128ecb7cf22606b50a81298c2
hitak77 <hitak77@naver.com>
2023-10-14 15:51:31 +0900

    level 0) 무작위로 K개의 수 뽑기

---
+++ randomK/src/main/kotlin/Solution.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun solution(arr: IntArray, k: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+
+        for(i in 0 until arr.size){
+            if(arr[i] !in answer){
+                answer += arr[i]
+            }
+            if(answer.size >= k) break
+        }
+        if(answer.size < k){
+            for(i in 0 until k-answer.size){
+                answer += -1
+            }
+        }
+
+        return answer
+    }
+}
+
+/**
+ * level 0) 무작위로 K개의 수 뽑기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181858
+ */
