361ea604b5d87601bec151fcdd61b960104f62ef
hitak77 <hitak77@naver.com>
2023-10-16 20:13:43 +0900

    level 0) 배열 만들기 5

---
+++ makeArray5/src/main/kotlin/Solution2.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun solution(intStrs: Array<String>, k: Int, s: Int, l: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+        val temp = s+l-1
+        for(i in 0 until intStrs.size){
+            val temp1 = intStrs[i].slice(s..temp).toInt()
+            if(temp1 > k){
+                answer += temp1
+            }
+
+        }
+
+        return answer
+    }
+}
+
+/**
+ * level 0) 배열 만들기 5
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181912
+ */
